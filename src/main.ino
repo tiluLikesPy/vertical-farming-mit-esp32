84c92f92e84983287c729c6d1a41fd931a6ba873
tiluLikesPy <90972477+tiluLikesPy@users.noreply.github.com>
2023-04-05 21:56:22 +0200

    05.04.2023
    In diesem commit wurde folgendes hinzugefügt:
    -das außlesen der Daten des DHT22
    -anhand der Luftfäuchtigkeit LEDs und Relays zu steuern

---
+++ src/main.ino
@@ -0,0 +1,126 @@
+#include <Arduino.h>
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
+
+// Definierung der Pins
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+#define PIN_RED_LED 2
+#define PIN_GREEN_LED 0
+
+const int relay1 = 32;
+const int relay2 = 33;
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// diese Funktion "definiert und startet die wichtigsten Prozesse"
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("DHTxx test!"));
+
+    dht.begin();
+
+
+    pinMode(PIN_RED_LED, OUTPUT);
+    pinMode(PIN_GREEN_LED, OUTPUT);
+
+    pinMode(relay1, OUTPUT);
+    pinMode(relay2, OUTPUT);
+
+
+}
+
+// diese Funktion läuft dauerhaft und "aktiviert" die unten angeführten Funktionen
+void loop() {
+  readDHT();
+  humidityControllLED();
+  controllThingWithHumidity();
+}
+
+
+//diese Funktion steuert die relays 1 und 2 abhängig von der Luftfeuchtigkeit (für genauere Erklärungen siehe Funktionsweiße)
+void controllThingWithHumidity(){
+  float h = dht.readHumidity();
+  if(h > 50){
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, LOW);
+  }
+  else if(h < 40) {
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, HIGH);
+  }
+  else{
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+  }
+}
+
+
+
+
+// diese Funktion steuert die leds anhand der Luftfeuchtigkeit (siehe Funktionsweiße)
+void humidityControllLED() {
+  float h = dht.readHumidity();
+  if (h>50){
+    digitalWrite(PIN_RED_LED, HIGH);
+    digitalWrite(PIN_GREEN_LED, LOW);
+  }
+  else {
+    digitalWrite(PIN_RED_LED, LOW);
+    digitalWrite(PIN_GREEN_LED, HIGH);
+  }
+}
+
+
+// diese Funktion gibt die Daten aus die vom DHT22 übermittelt werden. Außerdem werden diese daten auch über eine schnittstellen an einen angeschlossenen pc übermittelt
+void readDHT() {
+  delay(2000);
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  float hif = dht.computeHeatIndex(f, h);
+
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  //luftfeuchtigkeit
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  //temperatur
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+
+  //temperatur abhängig von der Luftfeuchtigkeit
+  Serial.print(F("   Heat index: "));
+  Serial.print(hic);
+  Serial.println(F("°C "));
+}
+
+// relay1 aktivieren/deactivieren
+void activateR1(){
+  digitalWrite(relay1, HIGH);
+}
+
+void stopR1(){
+  digitalWrite(relay1, LOW);
+}
+
+// relay2 aktivieren/deactivieren
+void activateR2(){
+  digitalWrite(relay2, HIGH);
+}
+
+void stopR2(){
+  digitalWrite(relay2, LOW);
+}
